9d154aee250e48a5be32caca77d3a64ed26fbe2d
Timur-Git <uchebapraktikum-t1@yandex.ru>
2024-09-04 01:26:50 +0300

    Async. registration, random target search with fixation and deletion are working successfully

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ import aiosqlite
 import copy
 
 bot = telebot.TeleBot('7292212331:AAHc8HtqomP8vidGw1o_9qcM6qJ860GDcMY')
-name = None
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     conn = sqlite3.connect('Killer.sql')
     cur = conn.cursor()
-    cur.execute(
-        'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mean TEXT, chat_id TEXT, target_id TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mean TEXT, chat_id TEXT, target_id TEXT)')
 
-    cur.execute(
-        'CREATE TABLE IF NOT EXISTS selected_users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mean TEXT, chat_id TEXT, target_id TEXT)')
-
+    cur.execute('CREATE TABLE IF NOT EXISTS selected_users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mean TEXT, chat_id TEXT, target_id TEXT)')
     conn.commit()
     cur.close()
     conn.close()
@@ -31,8 +28,7 @@ def start(message):
     List = types.KeyboardButton('Список')
     markup.row(Bin, List)
 
-
-    chat_id = f'{message.from_user.id}'
+    chat_id = f'{message.chat.id}'
 
     # Используем asyncio для запуска асинхронной проверки
     loop = asyncio.new_event_loop()
@@ -40,37 +36,34 @@ def start(message):
 
     # Проверка, существует ли пользователь
     if loop.run_until_complete(user_exists(chat_id)):
-        bot.reply_to(message, "Вы уже зарегистрированы!")
+        bot.send_message(message.chat.id, "Вы уже зарегистрированы!")
     else:
         bot.send_message(message.chat.id,
-                         f'Приветствую, {message.from_user.first_name}!\nТы попал на игру "Ловец душ" от МТУСИ\nDesigned by maksimator & tunknowng ',
+                         f'Приветствую, {message.from_user.first_name}!\nТы попал на игру "Собиратель душ" от МТУСИ\n_Designed by maksimator & tunknowng_',
+                         parse_mode='Markdown',
                          reply_markup=markup)
         bot.send_message(message.chat.id, 'Давай тебя зарегаем. Напиши свое ФИО')
-        bot.register_next_step_handler(message, username)
+        bot.register_next_step_handler(message, get_username)
 
 
 # Функция для получения имени пользователя и его регистрации
-def username(message):
+def get_username(message):
     name = message.text  # Имя пользователя, которое он отправил
-
-    # Сохраняем имя пользователя и переходим к запросу хобби
-    message.text = name
-    bot.reply_to(message, "Отлично! И мне нужно еще твое напрваление")
-    bot.register_next_step_handler(message, user_mean, name)
+    bot.send_message(message.chat.id, "Отлично! И мне нужно еще твое направление")
+    bot.register_next_step_handler(message, get_user_mean, name)
 
 
-# Функция для получения хобби пользователя и его регистрации
-def user_mean(message, name):
-    chat_id = message.from_user.id
-    mean = message.text  # Хобби, которое пользователь отправил
+# Функция для получения направления пользователя и его регистрации
+def get_user_mean(message, name):
+    mean = message.text
+    chat_id = message.chat.id
 
     # Используем asyncio для регистрации пользователя
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     loop.run_until_complete(register_user(name, mean, chat_id))
-    bot.reply_to(message, f"Ты зареган\nТвое кодовое слово: {message.from_user.id}")
-
+    bot.send_message(message.chat.id, f"Ты зареган\nТвое кодовое слово: {chat_id}")
 
 
 # Асинхронная функция для регистрации пользователя
@@ -82,17 +75,22 @@ async def register_user(name: str, mean: str, chat_id: str):
         )
         await db.commit()
 
+
 # Асинхронная функция для проверки существования пользователя
 async def user_exists(chat_id: str):
     async with aiosqlite.connect('Killer.sql') as db:
-        async with db.execute("SELECT 1 FROM users WHERE chat_id = ?", (chat_id,)) as cursor:
+        async with db.execute("SELECT 1 FROM users WHERE chat_id = ?",
+                              (chat_id,)) as cursor:
             exists = await cursor.fetchone()
-            if exists is None:
+            find = exists is not None
+            if find:
+                return find
+            else:
                 async with aiosqlite.connect('Killer.sql') as db:
-                    async with db.execute("SELECT 1 FROM selected_users WHERE chat_id = ?", (chat_id,)) as cursor:
+                    async with db.execute("SELECT 1 FROM selected_users WHERE chat_id = ?",
+                                          (chat_id,)) as cursor:
                         exists1 = await cursor.fetchone()
                         return exists1 is not None
-
 
 
 
@@ -101,49 +99,64 @@ def send(message):
     if message.text == 'Моя цель':
         conn = sqlite3.connect('Killer.sql')
         cur = conn.cursor()
-        exists = cur.execute("SELECT * FROM users WHERE chat_id = ?", (message.from_user.id,)).fetchone()
+        exists = cur.execute("SELECT * FROM users WHERE chat_id = ?",
+                             (message.chat.id,)).fetchone()
         cur.close()
         conn.close()
-        if exists:
-            conn = sqlite3.connect('Killer.sql')
-            cur = conn.cursor()
 
+        if exists:
             # Проверяем, есть ли у текущего пользователя уже закрепленный пользователь
-            cur.execute("SELECT target_id FROM users WHERE chat_id = ?", (message.from_user.id,))
-            target_id_row = cur.fetchone()
-            cur.close()
-            conn.close()
-            if target_id_row[0]:
-                bot.send_message(message.chat.id, 'Победа близка')
+            if exists[4]:
                 conn = sqlite3.connect('Killer.sql')
                 cur = conn.cursor()
-                cur.execute("SELECT name, mean FROM selected_users WHERE chat_id = ?", (target_id_row[0],))
-                assigned_user = cur.fetchone()
-                name, mean = assigned_user
+                assigned_user = cur.execute("SELECT * FROM selected_users WHERE chat_id = ?",
+                                            (exists[4],)).fetchone()
                 cur.close()
                 conn.close()
-                bot.send_message(message.chat.id, f"У вас уже закреплен пользователь: {name}, направление: {mean}")
+
+                if assigned_user:
+                    bot.send_message(message.chat.id, f"У вас уже закреплен пользователь: {assigned_user[1]}, направление: {assigned_user[2]}")
+                else:
+                    bot.send_message(message.chat.id, f"У вас уже был закреплен пользователь, но его душа неожиданно пропала")
+
+                    conn = sqlite3.connect('Killer.sql')
+                    cur = conn.cursor()
+                    cur.execute("UPDATE users SET target_id = ? WHERE chat_id = ?",
+                                (None, message.chat.id,))
+                    conn.commit()
+                    cur.close()
+                    conn.close()
             else:
                 select_users(message)
         else:
+            # Проверяем, есть ли у текущего пользователя уже закрепленный пользователь
             conn = sqlite3.connect('Killer.sql')
             cur = conn.cursor()
-
-            # Проверяем, есть ли у текущего пользователя уже закрепленный пользователь
-            cur.execute("SELECT target_id FROM selected_users WHERE chat_id = ?", (message.from_user.id,))
-            target_id_row = copy.deepcopy(cur.fetchone())
+            user = cur.execute("SELECT * FROM selected_users WHERE chat_id = ?",
+                               (message.chat.id,)).fetchone()
             cur.close()
             conn.close()
-            if target_id_row[0]:
-                bot.send_message(message.chat.id, 'лох не тот, кто лох, а тот, кто экономист')
+
+            if user and user[4]:
                 conn = sqlite3.connect('Killer.sql')
                 cur = conn.cursor()
-                cur.execute("SELECT name, mean FROM selected_users WHERE chat_id = ?", (target_id_row[0],))
-                assigned_user = cur.fetchone()
-                name, mean = assigned_user
+                assigned_user = cur.execute("SELECT * FROM selected_users WHERE chat_id = ?",
+                                            (user[4],)).fetchone()
                 cur.close()
                 conn.close()
-                bot.send_message(message.chat.id, f"У вас уже закреплен пользователь: {name}, направление: {mean}")
+
+                if assigned_user:
+                    bot.send_message(message.chat.id, f"У вас уже закреплен пользователь: {assigned_user[1]}, направление: {assigned_user[2]}")
+                else:
+                    bot.send_message(message.chat.id, f"У вас уже был закреплен пользователь, но его душа неожиданно пропала")
+
+                    conn = sqlite3.connect('Killer.sql')
+                    cur = conn.cursor()
+                    cur.execute("UPDATE selected_users SET target_id = ? WHERE chat_id = ?",
+                                (None, message.chat.id,))
+                    conn.commit()
+                    cur.close()
+                    conn.close()
             else:
                 select_users(message)
 
@@ -175,7 +188,8 @@ def send(message):
 def move_to_bin(message):
     conn = sqlite3.connect('Killer.sql')
     cur = conn.cursor()
-    cur.execute('DELETE FROM selected_users WHERE chat_id = ?', (message.text.strip(),))
+    cur.execute('DELETE FROM selected_users WHERE chat_id = ?',
+                (message.text.strip(),))
     conn.commit()
     cur.close()
     conn.close()
@@ -184,8 +198,8 @@ def move_to_bin(message):
 def select_users(message):
     conn = sqlite3.connect('Killer.sql')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM users WHERE NOT chat_id = ?', (message.from_user.id,))
-    users = cur.fetchall()
+    users = cur.execute('SELECT * FROM users WHERE NOT chat_id = ?',
+                        (message.from_user.id,)).fetchall()
     cur.close()
     conn.close()
 
@@ -193,43 +207,46 @@ def select_users(message):
         info = [[0 for j in range(4)] for i in range(len(users))]
         line = 0
         for el1 in users:
-            info[line][0] = f'{el1[1]}'
-            info[line][1] = f'{el1[2]}'
-            info[line][2] = f'{el1[3]}'
-            info[line][3] = f'{el1[4]}'
+            info[line][0] = el1[1]
+            info[line][1] = el1[2]
+            info[line][2] = el1[3]
+            info[line][3] = el1[4]
             line += 1
 
         selected_user = copy.deepcopy(random.choice(info))
         bot.send_message(message.chat.id, f'Имя: {selected_user[0]}\nНаправление: {selected_user[1]}')
+
         conn = sqlite3.connect('Killer.sql')
         cur = conn.cursor()
-        cur.execute("INSERT INTO selected_users (name, mean, chat_id, target_id) SELECT name, mean, chat_id, target_id FROM users" )
+        cur.execute("INSERT INTO selected_users (name, mean, chat_id, target_id) VALUES (?, ?, ?, ?)",
+                    (selected_user[0], selected_user[1], selected_user[2], selected_user[3],))
         conn.commit()
-        chat_id = selected_user[2]
-        exists = cur.execute("SELECT 1 FROM users WHERE chat_id = ?", [message.from_user.id]).fetchone()
+        exists = cur.execute("SELECT * FROM users WHERE chat_id = ?",
+                             (message.chat.id,)).fetchone()
         cur.close()
         conn.close()
+
         if exists:
             conn = sqlite3.connect('Killer.sql')
             cur = conn.cursor()
-
-            cur.execute("UPDATE users SET target_id = ? WHERE chat_id = ?", (chat_id, message.from_user.id,))
-
+            cur.execute("UPDATE users SET target_id = ? WHERE chat_id = ?",
+                        (selected_user[2], message.chat.id,))
             conn.commit()
             cur.close()
             conn.close()
         else:
             conn = sqlite3.connect('Killer.sql')
             cur = conn.cursor()
-
-            cur.execute("UPDATE selected_users SET target_id = ? WHERE chat_id = ?", (chat_id, message.from_user.id,))
-
+            cur.execute("UPDATE selected_users SET target_id = ? WHERE chat_id = ?",
+                        (selected_user[2], message.chat.id,))
             conn.commit()
             cur.close()
             conn.close()
+
         conn = sqlite3.connect('Killer.sql')
         cur = conn.cursor()
-        cur.execute('DELETE FROM users WHERE chat_id = ?', (selected_user[2],))
+        cur.execute('DELETE FROM users WHERE chat_id = ?',
+                    (selected_user[2],))
         conn.commit()
         cur.close()
         conn.close()
